7890e077da3bf1b6b93f5d12af604054e01ebd72
004329 <004329@ibar.int>
2026-04-22 19:04:54 +0400

    codecrafters submit [skip ci]

--- src/main/java/redis/Main.java
+++ src/main/java/redis/Main.java
@@ -8,30 +8,31 @@ import java.net.Socket;
 public class Main {
     static final String CLRF = "\r\n";
 
-    static void main(String[] args) {
-        Socket clientSocket = null;
+    public static void main(String[] args) {
         int port = 6379;
         try (ServerSocket serverSocket = new ServerSocket(port)) {
             // Since the tester restarts your program quite often, setting SO_REUSEADDR
             // ensures that we don't run into 'Address already in use' errors
             serverSocket.setReuseAddress(true);
             // Wait for connection from client.
-            clientSocket = serverSocket.accept();
-            InputStream inputStream = clientSocket.getInputStream();
-            byte[] buffer = new byte[1024];
-            while (inputStream.read(buffer) != -1) {
-                clientSocket.getOutputStream().write("+PONG\r\n".getBytes());
+            while (true) {
+                System.out.println("Waiting for connection...");
+                final Socket clientSocket = serverSocket.accept();
+                Thread.ofVirtual().start(() -> {
+                    try (clientSocket) {
+                        System.out.println("Connected to client");
+                        InputStream inputStream = clientSocket.getInputStream();
+                        byte[] buffer = new byte[1024];
+                        while (inputStream.read(buffer) != -1) {
+                            clientSocket.getOutputStream().write("+PONG\r\n".getBytes());
+                        }
+                    } catch (IOException e) {
+                        System.out.println("Error handling client: " + e.getMessage());
+                    }
+                });
             }
         } catch (IOException e) {
-            System.out.println("IOEx;jonkn hb,mj ,jmception: " + e.getMessage());
-        } finally {
-            try {
-                if (clientSocket != null) {
-                    clientSocket.close();
-                }
-            } catch (IOException e) {
-                System.out.println("IOException: " + e.getMessage());
-            }
+            System.out.println("IOException: " + e.getMessage());
         }
     }
 }
