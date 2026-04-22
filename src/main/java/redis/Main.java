d14facf262ce3703e8b1938622f9ed78aa4c463e
004329 <004329@ibar.int>
2026-04-22 18:31:34 +0400

    codecrafters submit [skip ci]

--- src/main/java/redis/Main.java
+++ src/main/java/redis/Main.java
@@ -1,12 +1,13 @@
 package redis;
 
 import java.io.IOException;
-import java.io.OutputStream;
+import java.io.InputStream;
 import java.net.ServerSocket;
 import java.net.Socket;
 
 public class Main {
     static final String CLRF = "\r\n";
+
     static void main(String[] args) {
         Socket clientSocket = null;
         int port = 6379;
@@ -16,10 +17,13 @@ public class Main {
             serverSocket.setReuseAddress(true);
             // Wait for connection from client.
             clientSocket = serverSocket.accept();
-            OutputStream outputStream = clientSocket.getOutputStream();
-            outputStream.write(("+PONG" + CLRF).getBytes());
+            InputStream inputStream = clientSocket.getInputStream();
+            byte[] buffer = new byte[1024];
+            while (inputStream.read(buffer) != -1) {
+                clientSocket.getOutputStream().write("+PONG\r\n".getBytes());
+            }
         } catch (IOException e) {
-            System.out.println("IOException: " + e.getMessage());
+            System.out.println("IOEx;jonkn hb,mj ,jmception: " + e.getMessage());
         } finally {
             try {
                 if (clientSocket != null) {
